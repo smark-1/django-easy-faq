1587fc863c8ad243107cde3e293a7caa87d8bd5f
dragoncommits <rabbit463@users.noreply.github.com>
2021-05-03 19:31:04 -0400

    added pypi release

--- setup.py
+++ setup.py
@@ -1,3 +1,23 @@
-from setuptools import setup
+from distutils.core import setup
+setup(
+  name = 'django-easy-faq',         # How you named your package folder (MyLib)
 
-setup()
+  version = '1.0',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  author = 'dragoncommits',                   # Type in your name
+  url = 'https://github.com/dragoncommits/django-easy-faq/',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/dragoncommits/django-easy-faq/archive/refs/tags/1.0.tar.gz',    # I explain this later on
+  keywords = ['django', 'FAQ', 'django-easy-faq','simple','frequently asked questions'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+          'django',
+      ],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which python versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
