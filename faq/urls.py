d40e036e7a8d6053c9d436abe8d73e67aa02665c
Muthu Annamalai <photonique@hey.com>
2023-02-12 23:20:55 -0800

    add login_required decorator

--- faq/urls.py
+++ faq/urls.py
@@ -2,26 +2,41 @@ from django.urls import path
 from . import views
 from . import path_converters
 from django.conf import settings
+from django.contrib.auth.decorators import login_required
 
 app_name = 'faq'
 
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index_view"),
+    path("", login_required(views.IndexView.as_view()), name="index_view"),
 ]
 
 # if using categories
 if "no_category" not in settings.FAQ_SETTINGS:
     urlpatterns += [
-        path("<uslug:slug>/", views.CategoryDetail.as_view(), name="category_detail"),
-        path("<uslug:slug>/add/question/", views.AddQuestion.as_view(), name="add_question"),
-        path("<uslug:slug>/<uslug:question>/", views.QuestionDetail.as_view(), name="question_detail"),
-        path("<uslug:category>/<uslug:question>/answer/", views.AddAnswer.as_view(), name="answer_question"),
+        path("<uslug:slug>/",
+             login_required(views.CategoryDetail.as_view()),
+             name="category_detail"),
+        path("<uslug:slug>/add/question/",
+             login_required(views.AddQuestion.as_view()),
+             name="add_question"),
+        path("<uslug:slug>/<uslug:question>/",
+             login_required(views.QuestionDetail.as_view()),
+             name="question_detail"),
+        path("<uslug:category>/<uslug:question>/answer/",
+             login_required(views.AddAnswer.as_view()),
+             name="answer_question"),
     ]
 else:
     urlpatterns += [
-        path("<uslug:slug>/", views.QuestionDetail.as_view(), name="question_detail"),
-        path("<uslug:question>/answer/", views.AddAnswer.as_view(), name="answer_question"),
-        path("add/question", views.AddQuestion.as_view(), name="add_question"),
+        path("<uslug:slug>/",
+             login_required(views.QuestionDetail.as_view()),
+             name="question_detail"),
+        path("<uslug:question>/answer/",
+             login_required(views.AddAnswer.as_view()),
+             name="answer_question"),
+        path("add/question",
+             login_required(views.AddQuestion.as_view()),
+             name="add_question"),
     ]
 
 # if using comments
@@ -29,11 +44,15 @@ if "no_comments" not in settings.FAQ_SETTINGS:
     # if using categories
     if "no_category" not in settings.FAQ_SETTINGS:
         urlpatterns += [
-            path("<uslug:category>/<uslug:question>/add/comment/", views.AddComment.as_view(), name="add_comment"),
+            path("<uslug:category>/<uslug:question>/add/comment/",
+                 login_required(views.AddComment.as_view()),
+                 name="add_comment"),
         ]
     else:
         urlpatterns += [
-            path("<uslug:question>/add/comment/", views.AddComment.as_view(), name="add_comment"),
+            path("<uslug:question>/add/comment/",
+                 login_required(views.AddComment.as_view()),
+                 name="add_comment"),
         ]
 
 # if using votes
@@ -41,13 +60,19 @@ if "no_votes" not in settings.FAQ_SETTINGS:
     # if using categories
     if "no_category" not in settings.FAQ_SETTINGS:
         urlpatterns += [
-            path("<uslug:category>/<uslug:question>/<uslug:answer>/vote/", views.VoteAnswerHelpful.as_view(),
+            path("<uslug:category>/<uslug:question>/<uslug:answer>/vote/",
+                 login_required(views.VoteAnswerHelpful.as_view()),
                  name="vote_answer"),
-            path("<uslug:category>/<uslug:question>/vote/", views.VoteQuestionHelpful.as_view(), name="vote_question")
+            path("<uslug:category>/<uslug:question>/vote/",
+                 login_required(views.VoteQuestionHelpful.as_view()),
+                 name="vote_question")
         ]
     else:
         urlpatterns += [
-            path("<uslug:question>/<uslug:answer>/vote/", views.VoteAnswerHelpful.as_view(),
+            path("<uslug:question>/<uslug:answer>/vote/",
+                 login_required(views.VoteAnswerHelpful.as_view()),
                  name="vote_answer"),
-            path("<uslug:question>/vote/", views.VoteQuestionHelpful.as_view(), name="vote_question")
+            path("<uslug:question>/vote/",
+                 login_required(views.VoteQuestionHelpful.as_view()),
+                 name="vote_question")
         ]
