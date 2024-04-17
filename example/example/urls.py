3efb35f2f721b533640de6fad44b4f73cc38d3ad
smark-1 <75799735+smark-1@users.noreply.github.com>
2024-04-16 22:38:55 -0400

    Add example app

---
+++ example/example/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from django.contrib import admin
+from home.views import home
+
+urlpatterns = [
+    path("", home),
+    path('faq/', include('faq.urls')),
+    path("admin/", admin.site.urls),
+]
+
